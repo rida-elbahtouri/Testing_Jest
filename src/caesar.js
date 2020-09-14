a28c4e5468d493167a78c86c22f4ce33e7d8737f
rida-elbahtouri <elbahtouri0123@gmail.com>
2020-09-14 12:02:25 +0100

    create caesar function

---
+++ src/caesar.js
@@ -0,0 +1,42 @@
+const Caesar = (str) => {
+  const caesar_obj = {
+    a: 'b',
+    b: 'c',
+    c: 'd',
+    d: 'e',
+    e: 'f',
+    f: 'g',
+    g: 'h',
+    h: 'i',
+    i: 'j',
+    j: 'k',
+    k: 'l',
+    l: 'm',
+    m: 'n',
+    n: 'o',
+    o: 'p',
+    p: 'q',
+    q: 'r',
+    r: 's',
+    s: 't',
+    t: 'u',
+    u: 'v',
+    v: 'w',
+    w: 'x',
+    x: 'y',
+    y: 'z',
+    z: 'a',
+  };
+  let newstr = str.split('');
+  let caesar = [];
+  newstr.forEach((letter) => {
+    if (letter != ' ') {
+      caesar.push(caesar_obj[letter]);
+    } else {
+      caesar.push(' ');
+    }
+  });
+
+  return caesar.join('');
+};
+export default Caesar;
