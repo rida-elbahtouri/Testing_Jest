e98ffb52ea1a146d760287e7c7a9fcf60a2fe5b0
rida-elbahtouri <elbahtouri0123@gmail.com>
2020-09-14 13:21:17 +0100

    add shift to caesar function

--- src/caesar.js
+++ src/caesar.js
@@ -1,37 +1,45 @@
-const Caesar = (str) => {
+const Caesar = (str, shift) => {
   const caesar_obj = {
-    a: 'b',
-    b: 'c',
-    c: 'd',
-    d: 'e',
-    e: 'f',
-    f: 'g',
-    g: 'h',
-    h: 'i',
-    i: 'j',
-    j: 'k',
-    k: 'l',
-    l: 'm',
-    m: 'n',
-    n: 'o',
-    o: 'p',
-    p: 'q',
-    q: 'r',
-    r: 's',
-    s: 't',
-    t: 'u',
-    u: 'v',
-    v: 'w',
-    w: 'x',
-    x: 'y',
-    y: 'z',
-    z: 'a',
+    a: 1,
+    b: 2,
+    c: 3,
+    d: 4,
+    e: 5,
+    f: 6,
+    g: 7,
+    h: 8,
+    i: 9,
+    j: 10,
+    k: 11,
+    l: 12,
+    m: 13,
+    n: 14,
+    o: 15,
+    p: 16,
+    q: 17,
+    r: 18,
+    s: 19,
+    t: 20,
+    u: 21,
+    v: 22,
+    w: 23,
+    x: 24,
+    y: 25,
+    z: 26,
+  };
+  const getKeyByValue = (object, value) => {
+    return Object.keys(object).find((key) => object[key] === value);
   };
   let newstr = str.split('');
   let caesar = [];
   newstr.forEach((letter) => {
     if (letter != ' ') {
-      caesar.push(caesar_obj[letter]);
+      let index = caesar_obj[letter] + shift;
+      if (index < 26) {
+        caesar.push(getKeyByValue(caesar_obj, index));
+      } else {
+        caesar.push(getKeyByValue(caesar_obj, index - 26));
+      }
     } else {
       caesar.push(' ');
     }
@@ -39,4 +47,6 @@ const Caesar = (str) => {
 
   return caesar.join('');
 };
+console.log(Caesar('attack at dawn z', 5));
+
 export default Caesar;
